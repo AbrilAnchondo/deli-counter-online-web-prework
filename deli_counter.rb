b3919c706f72e7c2e4dce1a2f76fb78f8f6c7d3f
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-05-09 01:09:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,7 @@
 # Write your code here.
 
 katz_deli = []
+$counter = 0
 
 def line(katz_deli)
    
@@ -13,21 +14,27 @@ def line(katz_deli)
       current_line  += " #{i+1}. #{x}"}
       puts "The line is currently:" + current_line
     end 
+    
 end
   
+#def take_a_number(katz_deli, new_person)
+#    if katz_deli.length >= 0 
+#       katz_deli.push(new_person)
+#       puts "Welcome, #{new_person}. You are number #{katz_deli.length} #in line."
+    #end
+#end
+
 def take_a_number(katz_deli, new_person)
-    if katz_deli.length >= 0 
-       katz_deli.push(new_person)
-       puts "Welcome, #{new_person}. You are number #{katz_deli.length} in line."
-    end
+  katz_deli.push(new_person)
+  $counter += 1 
+  puts "Welcome, you are ticket number #{$counter}."
 end
 
 def now_serving(katz_deli)
-  if katz_deli.length === 0
-    puts "There is nobody waiting to be served!"
-  end
   if katz_deli.length > 0
      puts "Currently serving #{katz_deli.first}."
-    katz_deli.shift
+    katz_deli.shift()
+  else
+    puts "There is nobody waiting to be served!"
   end
 end
